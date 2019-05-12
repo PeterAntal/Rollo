35374f80b8bf8aee132d0a039da0bc85607e94f2
PeterAntal <172160+PeterAntal@users.noreply.github.com>
2019-05-12 14:34:00 -0700

    Updates

---
+++ precache-manifest.07a95e2de2a729cae2c9d26b4907eb20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdde1f2281b1692d7181a04333caab3",
+    "url": "/Rollo/index.html"
+  },
+  {
+    "revision": "e7b1c8d487d8a4da5d1c",
+    "url": "/Rollo/static/css/main.986bf13c.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Rollo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e7b1c8d487d8a4da5d1c",
+    "url": "/Rollo/static/js/main.33f4baf2.chunk.js"
+  },
+  {
+    "revision": "fb2423df6e339da3b7ac",
+    "url": "/Rollo/static/js/runtime~main.1750d291.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Rollo/static/media/logo.ee7cd8ed.svg"
+  }
+]);
